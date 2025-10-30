019d734db329a3adff2655632bd8099b537a81a5
YueChengU <yue.cheng@pharm.utah.edu>
2025-10-30 12:14:58 -0600

    Add files via upload

---
+++ Data_Cleaning_with_Pandas.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[115]:
+
+
+#Data Cleaning - 1
+#Importing libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+#%% Importing Data
+flights_data = pd.read_csv('flights.csv')
+flights_data.head(10)
+weather_data_pd = pd.read_csv('weather.csv')
+weather_data_np = weather_data_pd.to_numpy()
+
+
+# In[127]:
+
+
+#use flights_data
+#Question 1 How many flights were there from JFK to SLC? Int q_1
+#Quick check to the data
+print('Question 1:')
+flights_data.head(10)
+#Use mask to filter all the  flights that met the description
+#Here I learned that Python's operator precedence - & and | have HIGHER precedence than comparison operators like >, ==, <. So parentheses are used to clearify the conditions.
+mask = (flights_data['origin'] == 'JFK') & (flights_data['dest'] == 'SLC')
+#Since mask will return a boolean value of 1 and 0, the sum will be the number of the flights that meet criteria.
+q_1 = int((mask).sum())
+print(q_1)
+print()
+
+
+# In[128]:
+
+
+#Question 2 How many airlines fly to SLC? Should be int
+# To answer this we could filter all the flights to SLC then find the number of unique value of airlines
+print('Question 2:')
+mask2 = flights_data['dest'] == 'SLC'
+flightstoSLC = flights_data[mask2]
+#Count the unique value of 'carrier' using nunique()
+q_2 = flightstoSLC['carrier'].nunique()
+print(q_2)
+print()
+
+
+# In[129]:
+
+
+#Question 3 What is the average arrival delay for flights to RDU? float
+print('Question 3:')
+flights_data.head(10)
+#Get all the flights to RDU first, then calculate the mean of the delay time using .mean()
+mask3 = flights_data['dest'] == 'RDU'
+flightstoRDU = flights_data[mask3]
+flightstoRDU['arr_delay'].mean()
+q_3 = flightstoRDU['arr_delay'].mean()
+print(q_3)
+#Round the answer to 1 decimal and cast it to standard float format
+q_3 = float(round(q_3, 1))
+print(q_3)
+print()
+
+
+# In[130]:
+
+
+#Question 4 What proportion of flights to SEA come from the two NYC airports (LGA and JFK)? float
+#Keep using mask to filter for the flights.
+#Total flights to SEA:
+print('Question 4:')
+mask4_1 = flights_data['dest'] == 'SEA'
+flightstoSEA = flights_data[mask4_1]
+#Flights to SEA that were coming from NYC airports
+mask4_2 = (flightstoSEA['origin'] == 'LGA') | (flightstoSEA['origin'] == 'JFK')
+#The proportion will be the sum of boolean values for NYC to SEA flights over that of total flights to 
+q_4 = (mask4_2).sum() / (mask4_1).sum()
+#Round the answer to 2 decimal and cast it to standard float format
+q_4 = float(round(q_4, 2))
+print(q_4)
+print()
+
+
+# In[131]:
+
+
+#Question 5 Which date has the largest average depature delay? Pd slice with date and float
+#please make date a column. Preferred format is 2013/1/1 (y/m/d)
+#Pandas have a function that could directly convert data into date form: pd.to_datetime()
+#First define the data of year, month and day
+print('Question 5:')
+flights_data['date'] = pd.to_datetime(flights_data[['year', 'month', 'day']])
+#Then convert to the preferred format using .strftime()
+flights_data['date_string'] = flights_data['date'].dt.strftime('%Y/%m/%d')
+#Quick check
+flights_data['date_string'].head()
+#Then calculate the mean values of the departure delay grouped by date
+average_dep_delay = flights_data.groupby('date_string')['dep_delay'].mean().reset_index()
+#Rename the mean value column for clarity
+average_dep_delay.columns = ['date_string', 'average_dep_delay']
+average_dep_delay.head()
+#Then sort the data by the average depature delays and rank from largest to smallest, then the date at the first row will be the day
+average_dep_delay = average_dep_delay.sort_values('average_dep_delay', ascending= False)
+q_5 = average_dep_delay['date_string'].iloc[0]
+print(f'The date that has the largest average depature delay was {average_dep_delay['date_string'].iloc[0]}, an average delay of {average_dep_delay['average_dep_delay'].iloc[0]}!')
+print(q_5)
+print()
+
+
+# In[132]:
+
+
+#Question 6 Which date has the largest average arrival delay? pd slice with date and float
+#Basically the same like question 5, just switch to the mean of arrival delay:
+print('Question 6:')
+average_arr_delay = flights_data.groupby('date_string')['arr_delay'].mean().reset_index()
+#Rename the mean value column for clarity
+average_arr_delay.columns = ['date_string', 'average_arr_delay']
+average_arr_delay.head()
+#Then sort the data by the average arrival delays and rank from largest to smallest, then the date at the first row will be the day
+average_arr_delay = average_arr_delay.sort_values('average_arr_delay', ascending= False)
+q_6 = average_arr_delay['date_string'].iloc[0]
+print(f'The date that has the largest average arrival delay was {average_arr_delay['date_string'].iloc[0]}, an average delay of {average_arr_delay['average_arr_delay'].iloc[0]}!')
+print(q_6)
+print()
+
+
+# In[133]:
+
+
+#Question 7 Which flight departing LGA or JFK in 2013 flew the fastest? pd slice with tailnumber and speed
+#speed = distance/airtime
+#Calculate the speed first
+print('Question 7:')
+flights_data['speed'] = (flights_data['distance']) / (flights_data['air_time'])
+#Sort the flights departing from NYC by the speed and rank from largest to smallest, then the tailnumber at the first row will be the flight
+mask_7 = (flights_data['origin'] == 'LGA') | (flights_data['origin'] == 'JFK')
+fastestflights = flights_data[mask_7].sort_values('speed', ascending = False)
+q_7 = fastestflights['tailnum'].iloc[0]
+print(f'The fastest flight departing from LGA or JFK in 2013 was flight {fastestflights['tailnum'].iloc[0]}, with a speed of {fastestflights['speed'].iloc[0]}.')
+print(q_7)
+print()
+
+
+# In[134]:
+
+
+#Question 8 Replace all nans in the weather pd dataframe with 0s. Pd with no nans
+#Just use .fillna(0) to replace nulls with 0, inplace-True very important to make sure the changes were applied to the original dataframe
+print('Question 8:')
+weather_data_pd.fillna(0, inplace = True)
+#Check if there's still nan
+q_8 = weather_data_pd.isnull().sum()
+print(q_8)
+print()
+
+
+# In[135]:
+
+
+#Question 9 How many observations were made in Feburary? Int
+#Since each roll is an observation, we just need to count all the rows with '2' as the month
+#Check datatype
+#print(weather_data_np.dtype)
+#The array was unstructured, using column indices for mask, month is the 4th column on the sheet
+print('Question 9:')
+mask_9 = weather_data_np[:, 3] == 2
+#Sum up the true boolean value of mask_9:
+q_9 = int(mask_9.sum())
+print(q_9)
+print()
+
+
+# In[136]:
+
+
+#Question 10 What was the mean for humidity in February? Float
+#Humidity is the 9th column, calculate the mean in the array of feburary observation from question 9
+#There were two observation at JFK besides the majority of observation at EWR.
+#I'm including the values in mean calculation since it's not specified to only calculate the humidity at EWR.
+print('Question 10:')
+q_10 = float(np.mean(weather_data_np[mask_9][:, 8]))
+print(q_10)
+print()
+
+
+# In[137]:
+
+
+#Question 11 What was the std for humidity in February? Float
+#Same way to select the column as question 10
+#The humidity observations at JFK were also included in the calculation
+print('Question 11:')
+q_11 = float(np.std(weather_data_np[mask_9][:,8]))
+print(q_11)
+
+
+# In[ ]:
+
+
+
+
---
+++ Pandas-2.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[73]:
+
+
+#Pandas Assignment 2
+#Question 1
+#Compute the euclidean distance between series (points) p and q, without using a packaged formula.
+#eculidean distance is the squre root of the sum of squared differences
+print('Question 1:')
+p = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+q = pd.Series([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+#Use **0.5 for square root calculation
+q_1 = ((p-q)**2).sum()**0.5
+print(f"The euclidean distance between p and d is {q_1}")
+print(q_1)
+print()
+
+
+# In[74]:
+
+
+#Question 2
+#Change the order of columns of a dataframe. Interchange columns 'a' and 'c'.
+print('Question 2:')
+df = pd.DataFrame(np.arange(20).reshape(-1, 5), columns=list('abcde'))
+#Manually assign new order of the columns to the dataframe
+
+df = df[['c','b','a','d','e']]
+#Or just swap two columns by their index
+#columns = df.columns.tolist()
+#columns[0], columns[2] = columns[2], columns[0]
+#df = df[columns]
+print(df)
+print()
+
+
+# In[75]:
+
+
+#Question 3
+#Change the order of columns of a dataframe.  Create a generic function to interchange two columns, without hardcoding column names.
+print('Question 3:')
+df = pd.DataFrame(np.arange(20).reshape(-1, 5), columns=list('abcde'))
+def column_interchange(df, col1, col2):
+    #Create a list of the column names
+    cols = df.columns.tolist()
+    #Get the index of the columns we want to interchange
+    if col1 in cols and col2 in cols:
+        col1_index = cols.index(col1)
+        col2_index = cols.index(col2)
+    #This creates a tuple of columns then change the position of certain index of the columns
+    cols[col1_index], cols[col2_index] = cols[col2_index], cols[col1_index]
+
+    return df[cols]
+
+print(column_interchange(df, 'a', 'c'))
+print()
+
+
+# In[76]:
+
+
+#Question 4
+#Format or suppress scientific notations in a pandas dataframe. Suppress scientific notations like ‘e-03’ in df and print upto 4 numbers after decimal.
+print('Question 4:')
+df = pd.DataFrame(np.random.random(4)**10, columns=['random'])
+
+#Set the presentation to be in 4 decimal float form
+pd.set_option('display.float_format', '{:.4f}'.format)
+print(df)
+print()
+
+
+# In[71]:
+
+
+#Question 5 
+#Create a new column that contains the row number of nearest column by euclidean distance. Create a new column such that, each row contains the row number of nearest row-record by euclidean distance.
+print('Question 5:')
+df = pd.DataFrame(np.random.randint(1,100, 40).reshape(10, -1), columns=list('pqrs'), index=list('abcdefghij'))
+#Consider rows as series. We need to calculate the euclidean distance of each row with all the other rows, then put the row number of the smallest distance to the new column
+#for-loop will be suitable for repeated iteration like this
+
+#Prepare an empty lists to hold all the values for the new columns
+nearest_row = []
+dist = []
+#This data frame has row index labeled differently than the regular number index ('a', 'b', 'c' instead of '0','1','2'). Will first catch all the row index into a list to iterate over later
+row_index = df.index
+#print(row_index)
+for i in row_index:
+    #Use .loc() to select rows, loc() is for label-based index while iloc()is for integer-based index
+    current_row = df.loc[i]
+    #Set the minimum distance to infinity to start the comparison with the actual calculation
+    min_distance = float('inf')
+    nearest_index = None
+
+    'd'#Make sure to exclud the row itself in the comparison
+    for j in row_index:
+        if i != j:
+            compare_row = df.loc[j]
+            distance = ((current_row-compare_row)**2).sum()**0.5
+            if distance < min_distance:
+                min_distance = distance
+                nearest_index = j
+    #append the nearest row number and distance to the column list
+    nearest_row.append(nearest_index)
+    dist.append(min_distance)
+
+
+#Add the new column to the data frame
+df['nearest_row'] = nearest_row
+df['dist'] = dist
+
+print(df)
+print()       
+
+
+# In[77]:
+
+
+#Question 6 
+#Correlation is a statistical technique that shows how two variables are related. Pandas dataframe.corr() method is used for creating the correlation matrix. It is used to find the pairwise correlation of all columns in the dataframe. Any na values are automatically excluded. For any non-numeric data type columns in the dataframe it is ignored.
+print('Question 6:')
+data = {'A': [45, 37, 0, 42, 50],
+        'B': [38, 31, 1, 26, 90],
+        'C': [10, 15, -10, 17, 100],
+        'D': [60, 99, 15, 23, 56],
+        'E': [76, 98, -0.03, 78, 90]
+        }
+#Testing the df.corr() out on the provided dataframe
+df = pd.DataFrame(data)
+
+print(df.corr())
+
+
+# In[ ]:
+
+
+
+
